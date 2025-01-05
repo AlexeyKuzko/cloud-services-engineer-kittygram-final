f883974f1225c857b121db3ab073424f2b3ccd7f
AlexeyKuzko <kuzkoalexey@icloud.com>
2025-01-05 10:13:26 +0300

    refactor: clean up imports and improve formatting in models, serializers, and views

--- backend/cats/serializers.py
+++ backend/cats/serializers.py
@@ -63,29 +63,33 @@ class CatSerializer(serializers.ModelSerializer):
         return dt.datetime.now().year - obj.birth_year
 
     def create(self, validated_data):
-        if "achievements" not in self.initial_data:
+        if "achievements" not in self.initial_data:  # type: ignore
             cat = Cat.objects.create(**validated_data)
             return cat
         else:
             achievements = validated_data.pop("achievements")
             cat = Cat.objects.create(**validated_data)
             for achievement in achievements:
-                current_achievement, status = Achievement.objects.get_or_create(
+                current_achievement, _ = Achievement.objects.get_or_create(
                     **achievement
                 )
-                AchievementCat.objects.create(achievement=current_achievement, cat=cat)
+                AchievementCat.objects.create(
+                    achievement=current_achievement, cat=cat
+                )
             return cat
 
     def update(self, instance, validated_data):
         instance.name = validated_data.get("name", instance.name)
         instance.color = validated_data.get("color", instance.color)
-        instance.birth_year = validated_data.get("birth_year", instance.birth_year)
+        instance.birth_year = validated_data.get(
+            "birth_year", instance.birth_year
+        )
         instance.image = validated_data.get("image", instance.image)
         if "achievements" in validated_data:
             achievements_data = validated_data.pop("achievements")
             lst = []
             for achievement in achievements_data:
-                current_achievement, status = Achievement.objects.get_or_create(
+                current_achievement, _ = Achievement.objects.get_or_create(
                     **achievement
                 )
                 lst.append(current_achievement)
